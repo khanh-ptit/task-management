4182cbc09cfe09c5bae24922b85e9250929a093c
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-19 18:25:47 +0700

    Bộ lọc trạng thái

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,12 +1,18 @@
 const Task = require("../models/task.model")
 
+// [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
-    const tasks = await Task.find({
+    let find = {
         deleted: false
-    })
+    }
+    if (req.query.status) {
+        find.status = req.query.status
+    }
+    const tasks = await Task.find(find)
     res.json(tasks)
 }
 
+// [GET] /api/v1/tasks/detail/:id
 module.exports.detail = async (req, res) => {
     try {
         const id = req.params.id
@@ -18,5 +24,4 @@ module.exports.detail = async (req, res) => {
     } catch (error) {
         res.json("Không tồn tại bản ghi")
     }
-    
 }
