2e35caf4a55fb8ecae56ecc5bc2429f9cd8658ad
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-21 16:31:30 +0700

    cài đặt cors

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ require("dotenv").config()
 const port = process.env.PORT
 const database = require("./config/database")
 database.connect()
+var cors = require("cors")
 
 // Nếu sau này lỗi thì install body-parse (nhớ lên web đọc doc trên npm)
 app.use(express.json());
+app.use(cors())
 
 const route = require("./api/v1/routes/index.route")
 route(app)
