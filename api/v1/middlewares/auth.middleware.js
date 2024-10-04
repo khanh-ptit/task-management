9fbc23c9120462ad14ab5f5d9c470cc969e34d21
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-10-05 00:16:45 +0700

    Update auth.middleware.js

--- api/v1/middlewares/auth.middleware.js
+++ api/v1/middlewares/auth.middleware.js
@@ -1,8 +1,8 @@
 const User = require("../models/user.model")
 
 module.exports.requireAuth = async (req, res, next) => {
-    if (req.cookies.token) {
-        const token = req.cookies.token
+    if (req.headers.authorization) {
+        const token = req.headers.authorization.split(" ")[1]
         // console.log(token)
 
         const user = await User.findOne({
